805589440e1349646d04bea6ea8b33e864884d6c
Apoorv Garg <u.garg.10@gmail.com>
2020-11-28 22:11:12 +0530

    Basic

---
+++ FlaskTut/lect_1.py
@@ -0,0 +1,18 @@
+from flask import Flask,redirect,url_for
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+@app.route("/<name>")
+def user(name):
+    return f'Hello {name}'
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user",name="Apoorv"))
+
+if __name__=="__main__":
+    app.run()
